4292d7bdf2aa281415150afb4afc80f3c4368a63
HIRAI, Takayuki <takayuki.hirai@nex.nikkei.com>
2024-12-19 17:16:57 +0900

    second

--- main.go
+++ main.go
@@ -13,5 +13,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to marshal string: %v", err)
 	}
+
+    var data string
+    json := []byte("\"Hello, world!\"")
+    err = sonic.Unmarshal(json, &data)
+	if err != nil {
+		log.Fatalf("Failed to unmarshal json: %v", err)
+	}
+
 	fmt.Println(string(bs))
 }
